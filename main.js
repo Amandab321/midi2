afc97c54dc15af3313ef5ea5192b3e24d1073f3c
Amandab321 <133907544+Amandab321@users.noreply.github.com>
2023-10-26 14:22:34 -0300

    código

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 alert ('Olá mundo!')
+function Tocasom (seletorAudio) { 
+    const elemento = document.querySelector (seletorAudio);
+    if (elemento) && elemento.localName=== 'Audio') {
+    elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+    }
+}
